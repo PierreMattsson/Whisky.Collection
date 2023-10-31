92a1d4a7d7b8604208bd12458fd1d64a67bfb303
Pierre Mattsson <pierre.mattsson87@gmail.com>
2023-10-31 14:37:48 +0100

    Added comment

--- Whisky.Collection.Application/Features/MyWhisky/Queries/GetAllMyWhisky/MyWhiskyDTO.cs
+++ Whisky.Collection.Application/Features/MyWhisky/Queries/GetAllMyWhisky/MyWhiskyDTO.cs
@@ -3,6 +3,7 @@
 public class MyWhiskyDTO
 {
     // This DTO is to show all Whisky so for this we only incloude the fields we want to see in a view state.
+    // So in this case we cam print out all the whisky with the Name of producer and whisky for each whisky.
     public int Id { get; set; }
     public string ProducerName { get; set; } = string.Empty;
     public string WhiskyName { get; set; } = string.Empty;
