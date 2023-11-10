7f504796bad8321d362f774f31a47d527244d3f3
Pierre Mattsson <pierre.mattsson87@gmail.com>
2023-11-10 15:49:13 +0100

    Added CREATE Command and Handler for MyWhisky

---
+++ Whisky.Collection.Application/Features/MyWhisky/Commands/CreateMyWhisky/CreateMyWhiskyCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Whisky.Collection.Application.Features.MyWhisky.Commands.CreateMyWhisky;
+
+public class CreateMyWhiskyCommand : IRequest<int>
+{
+    public string ProducerName { get; set; } = string.Empty;
+    public string WhiskyName { get; set; } = string.Empty;
+    public int WhiskyYearStatement { get; set; }
+    public string BottleDescription { get; set; } = string.Empty;
+    public double AlkoholProcent { get; set; }
+    public int BottleContentMilliliter { get; set; }
+}
---
+++ Whisky.Collection.Application/Features/MyWhisky/Commands/CreateMyWhisky/CreateMyWhiskyCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Whisky.Collection.Application.Contracts.Persistence;
+
+namespace Whisky.Collection.Application.Features.MyWhisky.Commands.CreateMyWhisky;
+
+public class CreateMyWhiskyCommandHandler : IRequestHandler<CreateMyWhiskyCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly IMyWhiskyRepository _myWhiskyRepository;
+
+    public CreateMyWhiskyCommandHandler(
+        IMapper mapper, 
+        IMyWhiskyRepository myWhiskyRepository)
+    {
+        _mapper = mapper;
+        _myWhiskyRepository = myWhiskyRepository;
+    }
+    public async Task<int> Handle(CreateMyWhiskyCommand request, CancellationToken cancellationToken)
+    {
+        // Validate incoming data
+
+        // Convert to domain entity object 
+        var myWhiskyToCreate = _mapper.Map<Domain.MyWhisky>(request);
+
+        // Add to database
+        await _myWhiskyRepository.CreateAsync(myWhiskyToCreate);
+
+        // Return record id
+        return myWhiskyToCreate.Id;
+    }
+}
