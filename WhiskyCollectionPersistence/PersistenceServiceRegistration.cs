0fbcb70ae0e1cffbbb6854022aa088a096f03b8d
Pierre Mattsson <pierre.mattsson87@gmail.com>
2023-11-14 16:51:14 +0100

    Added Persistence project and added its Service class

---
+++ WhiskyCollectionPersistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WhiskyCollectionPersistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
