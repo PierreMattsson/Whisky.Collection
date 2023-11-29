628239fd0f3e48a6c65c656a728079381ce4cf44
Pierre Mattsson <pierre.mattsson87@gmail.com>
2023-11-30 00:27:05 +0100

    asd

--- Whisky.Collection.BlazorUI/Program.cs
+++
@@ -1,9 +0,0 @@
-using Whisky.Collection.BlazorUI;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
